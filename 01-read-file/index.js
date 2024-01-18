9ddabdc78dd1652a47429cb7b60f8dc4269a980a
pasha spasibo <pa.spasibo@gmail.com>
2024-01-19 01:23:13 +0400

    feat: task1 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+ const fs = require('fs');
+ const path = require("node:path");
+ const myPath = path.normalize(__dirname + "/text.txt");
+ const stream = new fs.ReadStream(myPath, {encoding: "utf8"});
+ 
+ stream.on('readable', () => {
+    const data = stream.read();
+    console.log(data);
+ });
+ 
